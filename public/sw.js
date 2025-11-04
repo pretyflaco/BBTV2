da19e813d58200c7716e7a51b39d2c2c33f45a1c
pretyflaco <kemal@blink.sv>
2025-11-04 15:32:28 +0300

    Bump service worker cache version for NFC UX update

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Blink POS
-const CACHE_NAME = 'blink-tracker-v3-2025-11-04-nfc'; // Update this version when deploying changes
+const CACHE_NAME = 'blink-tracker-v4-2025-11-04-nfc-invoice'; // Update this version when deploying changes
 const urlsToCache = [
   '/',
   '/manifest.json',

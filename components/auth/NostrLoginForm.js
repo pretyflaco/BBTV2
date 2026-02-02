ea1728a9ca52640734a34ed460dcc51b5f2b0634
pretyflaco <kemal@blink.sv>
2026-02-02 18:08:25 +0300

    fix: add inline remote logger script for iOS (v40)
    - Add Script component with beforeInteractive strategy
    - Send log immediately via sendBeacon AND fetch
    - Runs before React hydration
    - Uses keepalive: true for fetch
    - Should work even if _app.js useEffect doesn't run

--- components/auth/NostrLoginForm.js
+++ components/auth/NostrLoginForm.js
@@ -16,7 +16,7 @@ import NostrConnectModal from './NostrConnectModal';
 
 // Build version - update this when deploying changes
 // This helps verify the correct build is running in the browser
-const BUILD_VERSION = 'v39-remote-log-fix';
+const BUILD_VERSION = 'v40-inline-remote-log';
 const BUILD_DATE = '2025-02-02';
 
 export default function NostrLoginForm() {

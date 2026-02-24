d5793349d3fd770221d1d3cbd28752049cd54225
CI blinkbitcoinbot <202112752+blinkbitcoinbot@users.noreply.github.com>
2026-02-24 11:55:20 +0000

    ci(shared): bump vendored ci files

---
+++ ci/vendor/tasks/run-on-nix-host.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -eu
+
+echo "    --> Executing run-on-nix-host.sh"
+echo "        running docker version: $(docker --version)"
+echo "        running docker compose version: $(docker compose version)"
+
+export CI_ROOT=$(pwd)
+
+host_name=$(cat docker-host/metadata | jq -r '.docker_host_name')
+echo "    --> Running on host: ${host_name}"
+host_zone=$(cat docker-host/metadata | jq -r '.docker_host_zone')
+gcp_project=$(cat docker-host/metadata | jq -r '.docker_host_project')
+
+gcloud_ssh() {
+  gcloud compute ssh ${host_name} \
+    --zone=${host_zone} \
+    --project=${gcp_project} \
+    --ssh-key-file=${CI_ROOT}/login.ssh \
+    --tunnel-through-iap \
+    --command "$@" 2> /dev/null
+}
+
+cat <<EOF > "${CI_ROOT}/gcloud-creds.json"
+${GOOGLE_CREDENTIALS}
+EOF
+cat <<EOF > "${CI_ROOT}/login.ssh"
+${SSH_PRIVATE_KEY}
+EOF
+chmod 600 "${CI_ROOT}/login.ssh"
+cat <<EOF > "${CI_ROOT}/login.ssh.pub"
+${SSH_PUB_KEY}
+EOF
+
+echo "    --> Activating gcloud service account"
+gcloud auth activate-service-account --key-file "${CI_ROOT}/gcloud-creds.json" 2> /dev/null
+
+echo "    --> Clean docker-host for execution"
+gcloud_ssh "docker ps -qa | xargs docker rm -fv || true; sudo rm -rf ${REPO_PATH} || true; mkdir -p ${REPO_PATH} && cd ${REPO_PATH}/../ && rmdir $(basename ${REPO_PATH})"
+
+pushd "${REPO_PATH}"
+
+[ -f .env.ci ] && envsubst < .env.ci > tmp.env.ci || true
+
+echo "    --> Copying repo to host"
+gcloud compute scp --ssh-key-file=${CI_ROOT}/login.ssh \
+  --recurse $(pwd) ${host_name}:${REPO_PATH} \
+  --tunnel-through-iap \
+  --zone=${host_zone} \
+  --project=${gcp_project} > /dev/null
+
+echo "    --> Build export statements for PARAMS_KEYS variables"
+export_vars=""
+if [ -n "${PARAMS_KEYS:-}" ]; then
+  for key in ${PARAMS_KEYS}; do
+    if [ -n "${!key:-}" ]; then
+      echo "    --> Exporting ${key}"
+      export_vars="${export_vars}export ${key}=\"${!key}\"; "
+    fi
+  done
+fi
+
+echo "    --> Executing command on host: cd ${REPO_PATH}; nix develop -c ${CMD}"
+gcloud_ssh "cd ${REPO_PATH}; ${export_vars}nix develop -c ${CMD} 2>&1"
